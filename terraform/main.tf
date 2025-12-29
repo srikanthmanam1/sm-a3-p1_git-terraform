e4f7601a99a6ccc55ab87cabcddf16589e04290f
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-12-28 20:37:42 -0600

    Update main.tf with port 8080

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,13 @@ resource "aws_security_group" "sm-std-sg1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description = "Custom TCP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
