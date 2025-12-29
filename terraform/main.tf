adc36f968c77ccde2d8bc903d2b97eab45f059aa
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-12-28 21:06:01 -0600

    Update main.tf Added HTTPS Port 443

--- terraform/main.tf
+++ terraform/main.tf
@@ -127,6 +127,13 @@ resource "aws_security_group" "sm-usw2-std-sg1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
